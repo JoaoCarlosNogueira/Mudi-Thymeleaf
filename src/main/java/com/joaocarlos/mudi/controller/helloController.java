b7cd4613c29d7f281cb92a954861de70d53c927b
João Carlos Nogueira <76751329+JoaoCarlosNogueira@users.noreply.github.com>
2022-11-01 23:45:54 -0400

    using model interface

--- src/main/java/com/joaocarlos/mudi/controller/helloController.java
+++ src/main/java/com/joaocarlos/mudi/controller/helloController.java
@@ -4,14 +4,15 @@ package com.joaocarlos.mudi.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class helloController {
 	
 	@GetMapping("/hello")
-	public String helo(HttpServletRequest request) {
-		request.setAttribute("nome", "teste");
+	public String helo(Model model) {
+		model.addAttribute("nome", "teste");
 		return "hello";
 	}
 }

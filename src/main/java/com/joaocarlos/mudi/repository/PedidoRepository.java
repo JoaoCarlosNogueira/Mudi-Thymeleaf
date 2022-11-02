a58cb64ebc5250648a6c04fdb9e1e59f25e39a2e
João Carlos Nogueira <76751329+JoaoCarlosNogueira@users.noreply.github.com>
2022-11-02 01:22:51 -0400

    configuring database connection

--- src/main/java/com/joaocarlos/mudi/repository/PedidoRepository.java
+++ src/main/java/com/joaocarlos/mudi/repository/PedidoRepository.java
@@ -1,6 +1,7 @@
 package com.joaocarlos.mudi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.joaocarlos.mudi.model.Pedido;
 

cfc9d84dcae49a7bb0f7f40f0c3515e002dbab02
João Carlos Nogueira <76751329+JoaoCarlosNogueira@users.noreply.github.com>
2022-11-02 01:30:37 -0400

    mapping Pedido and add new sources to PedicoController

--- src/main/java/com/joaocarlos/mudi/controller/helloController.java
+++ src/main/java/com/joaocarlos/mudi/controller/helloController.java
@@ -1,18 +1,24 @@
 package com.joaocarlos.mudi.controller;
 
-
-import javax.servlet.http.HttpServletRequest;
-
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.joaocarlos.mudi.model.Pedido;
+import com.joaocarlos.mudi.repository.PedidoRepository;
+
 @Controller
 public class helloController {
 	
-	@GetMapping("/hello")
-	public String helo(Model model) {
-		model.addAttribute("nome", "teste");
-		return "hello";
+	@Autowired
+	private PedidoRepository repository;
+	
+	@GetMapping("/home")
+	public String home(Model model) {
+		List<Pedido> pedidos = repository.findAll();
+		model.addAttribute("pedidos", pedidos);
+		return "home"; 
 	}
 }

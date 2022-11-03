7280af6d1ecc62b09d7cf842bb271a882ce5f204
João Carlos Nogueira <76751329+JoaoCarlosNogueira@users.noreply.github.com>
2022-11-02 22:02:39 -0400

    doing the last changes of the application

---
+++ src/main/java/com/joaocarlos/mudi/model/StatusPedido.java
@@ -0,0 +1,5 @@
+package com.joaocarlos.mudi.model;
+
+public enum StatusPedido {
+	AGUARDANDO, APROVADO, ENTREGUE;
+}
--- src/main/java/com/joaocarlos/mudi/repository/PedidoRepository.java
+++ src/main/java/com/joaocarlos/mudi/repository/PedidoRepository.java
@@ -1,11 +1,17 @@
 package com.joaocarlos.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.joaocarlos.mudi.model.Pedido;
+import com.joaocarlos.mudi.model.StatusPedido;
+
 
 @Repository
-public interface PedidoRepository  extends JpaRepository<Pedido, Long> {
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+	List<Pedido> findByStatus(StatusPedido aguardando);
+	
 }
